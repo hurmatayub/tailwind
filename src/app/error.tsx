f426b2c0856218d501bdb05de0ebf3d692af197d
hurmatayub <156796678+hurmatayub@users.noreply.github.com>
2024-10-17 23:45:54 +0500

    ..

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,8 +1,17 @@
 "use client"
+import Link from "next/link";
+
 export default function Error() {
-    return (
-        <div className="bg-[#A6B37D] border-8 border-black border-dotted h-screen flex items-center justify-center">
-            <h1 className="text-center text-4xl font-semibold">There is an <br /> error on this page</h1>
-        </div>
+    return(
+      <div className=" bg-[#A6B37D] h-screen flex flex-col justify-center items-center text-center">
+      <h1 className="text-4xl font-bold text-black mb-4">Something went wrong!</h1>
+      <p className="text-lg text-black mb-8">
+        We encountered an unexpected error. Please try again later.
+      </p>
+      <button
+        className="bg-[#B99470] hover:bg-[#C5705D] text-white font-bold py-2 px-4 rounded"><Link href="/">Try Again</Link>
+      </button>
+    </div>
     );
 }
+
--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,7 +1,12 @@
-export default function Notfound() {
+export default function NotFound() {
     return (
-        <div className="bg-[#A6B37D] border-8 border-black border-dotted h-screen flex items-center justify-center">
-            <h1 className="text-center text-4xl font-semibold">This Page Is<br />Not Found</h1>
-        </div>
+      <div className="bg-[#A6B37D] h-screen flex flex-col justify-center items-center text-center">
+        <h1 className="text-5xl font-bold text-black mb-4">404</h1>
+        <p className="text-xl text-black mb-8">Sorry, the page is does not exist.</p>
+        <a href="/" className="bg-[#B99470] hover:bg-[#C5705D] text-white font-bold py-2 px-4 rounded">
+          Go Back Home
+        </a>
+      </div>
     );
-}
+  }
+  
