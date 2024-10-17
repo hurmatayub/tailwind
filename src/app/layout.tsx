3a6691af06b291eb0cae40aa7865e369f29cd5f4
hurmatayub <156796678+hurmatayub@users.noreply.github.com>
2024-10-18 00:04:28 +0500

    ..

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import Navbar from "./componenets/navbar";
+import About from "./about/page";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
